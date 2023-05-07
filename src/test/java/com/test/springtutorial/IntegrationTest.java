461e94fb0345a9b30b7be5a079b03bb208ccaf3b
DESKTOP-A1STFDR\kienh <kien.ho997@gmail.com>
2023-05-07 17:46:27 +0700

    first commit

---
+++ src/test/java/com/test/springtutorial/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.test.springtutorial;
+
+import com.test.springtutorial.SpringtutorialApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = SpringtutorialApp.class)
+public @interface IntegrationTest {
+}
